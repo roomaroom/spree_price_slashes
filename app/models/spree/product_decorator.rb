2993acdd3ef4fb484ef13c81620c61bce59ed74d
Stuart Bates <stuart@builtbyclick.com>
2014-07-31 14:04:12 +0100

    Made discount related methods available on product instances.

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,13 @@
 Spree::Product.class_eval do
 
   attr_accessible :discounted_amount
-  delegate_belongs_to :master, :discounted_amount
+
+  delegate_belongs_to :master,
+                      :discounted_amount,
+                      :display_price_without_discounted_amount,
+                      :price_without_discounted_amount,
+                      :discount_as_percentage,
+                      :discount_as_money,
+                      :discounted?
 
 end
